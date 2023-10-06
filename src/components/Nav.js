c469662610ed60bfba9bc90aee6a9092ac06fb2c
Shane White <shane.white1308@gmail.com>
2023-10-06 17:07:50 +0530

    image added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,14 @@
-import React from "react";
+// import React from "react";
+let url = "https://picsum.photos/seed/picsum/200/300";
 
 const Nav = () => {
+  let value = Math.floor(Math.random() * 10);
   return (
     <>
       <div className="App">
+        <p>{value}</p>
+        {value === 7 ? <img src={url} /> : <h2>LOSS</h2>}
+        {/* {value === 5 && <img src={url} />} */}
         <h1>Hello World</h1>
       </div>
     </>
